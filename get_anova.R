6280d2b7cc726055876ff32fbd19ce5965eef5a7
olutz <olivia.joy.lutz@gmail.com>
2018-04-11 16:05:00 -0400

    Update get_anova.R

--- get_anova.R
+++ get_anova.R
@@ -7,8 +7,9 @@
 ## name of levels to compare within group variable ('SZ','HC') 
 ## Note can only handle two levels
 ## list of covariates ( covars=c('age','sex','race'))
+## method for adjusting p values (BH, fdr, bonferroni)
 
-get_anova<-function(data,group,group1, group2, measures,covars){
+get_anova<-function(data,group,group1, group2, measures,covars, method="BH"){
   result<-c()
   pval<-c()
   d<-c()
@@ -17,7 +18,7 @@ get_anova<-function(data,group,group1, group2, measures,covars){
     pval<-append(pval, result['Pr(>F)2'])
     d<-append(d, effect_size(data, meas,group, group1, group2,covars))
   }
-  adj_pval <- p.adjust(pval, method="BH")
+  adj_pval <- p.adjust(pval, method=method)
   out<-data.frame(measures,pval=round(pval,3),adj_pval=round(adj_pval,3),d)
   names(out)<-c("Region","p_value","p_value_adj",'Cohens_D')
   return(out)
